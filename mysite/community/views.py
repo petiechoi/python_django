c033bdaf6ff4e85e60fbfff336497120d6fa85e6
alice <alice@modim.co.kr>
2023-04-06 17:01:21 +0900

    2.장고의 기본요소 완료

--- mysite/community/views.py
+++ mysite/community/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Question
 from django.utils import timezone
+from .forms import QuestionForm, AnswerForm
 # Create your views here.
 
 def index(request):
@@ -28,6 +29,37 @@ def answer_create(request, question_id):
     답변 등록
     """
     question = get_object_or_404(Question, pk=question_id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('community:detail', question_id=question.id)
+    else:
+        form = AnswerForm()
+    context = {'question': question, 'form' : form}
+    return render(request, 'community/question_detail.html', context)
+
+
+
     question.answer_set.create(content=request.POST.get('content'),
                                create_date=timezone.now())
     return redirect('community:detail', question_id = question_id)
+
+def question_create(request):
+    """
+    질문 등록
+    """
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('community:index')
+    else:
+        form = QuestionForm()
+    context = {'form' : form}
+    return render(request, 'community/question_form.html', context)
