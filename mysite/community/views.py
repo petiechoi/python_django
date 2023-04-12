46b9787fc610e347b32c7063214ea418a3c96eb5
alice <alice@modim.co.kr>
2023-04-12 11:51:13 +0900

    03-9 삭제 수정 완료

--- mysite/community/views.py
+++ mysite/community/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Question
+from .models import Question, Answer
 from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
@@ -109,3 +109,38 @@ def question_delete(request, question_id):
         return redirect('community:detail', question_id = question.id)
     question.delete()
     return redirect('community:index')
+
+@login_required(login_url='common:login')
+def answer_modify(request, answer_id):
+    """
+    답변 수정
+    """
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error(request, '수정 권한이 없습니다.')
+        return redirect('community:detail', Answer_id=answer.question.id)
+
+    if request.method == 'POST':
+        form = AnswerForm(request.POST, instance=answer)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.author = request.user
+            answer.modify_date = timezone.now()
+            answer.save()
+            return redirect('community:detail', question_id=answer.question.id)
+    else:
+        form = AnswerForm(instance=answer)
+    context ={'answer':answer, 'form' : form}
+    return render(request, 'community/answer_form.html', context)
+
+@login_required(login_url='common:login')
+def answer_delete(request, answer_id):
+    """
+    답변 삭제
+    """
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error(request, '삭제권한이 없습니다')
+    else:
+        answer.delete()
+    return redirect('community:detail', question_id = answer.question.id)
