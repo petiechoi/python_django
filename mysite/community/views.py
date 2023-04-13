1394765d90ee8f236a066832703727e114f74733
alice <alice@modim.co.kr>
2023-04-13 12:09:12 +0900

    03-10 댓글 기능 추가하기 답변 댓글 완

--- mysite/community/views.py
+++ mysite/community/views.py
@@ -204,3 +204,60 @@ def comment_delete_question(request, comment_id):
     else :
         comment.delete()
     return redirect('community:detail', question_id = comment.question.id)
+
+@login_required(login_url='common:login')
+def comment_create_answer(request, answer_id):
+    """
+    답변 질문 등록
+    """
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.method == 'POST' :
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.create_date = timezone.now()
+            comment.answer = answer
+            comment.save()
+            return redirect('community:detail',question_id=comment.answer.question.id)
+    else:
+        form = CommentForm()
+    context = {'form' : form}
+    return render(request, 'community/comment_form.html', context)
+
+
+@login_required(login_url='common:login')
+def comment_modify_answer(request, comment_id):
+    """
+    답변 질문 수정
+    """
+    comment = get_object_or_404(Comment, pk = comment_id)
+    if comment.author != request.user:
+        messages.error(request, '댓글수정권한이 없습니다')
+        return redirect('community:detail', question_id=comment.answer.question.id)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.modify_date = timezone.now()
+            comment.save()
+            return redirect('community:detail', question_id = comment.answer.question.id)
+    else:
+        form = CommentForm(instance=comment)
+    context = {'form':form}
+    return render(request, 'community/comment_form.html', context)
+
+@login_required(login_url='common:login')
+def comment_delete_answer(request, comment_id):
+    """
+    답변 질문 삭제
+    """
+    comment = get_object_or_404(Comment, pk=comment_id)
+    if comment.author != request.user:
+        messages.error(request, '댓글삭제권한이 없습니다')
+        return redirect('community:detail', question_id=comment.answer.question.id)
+    else:
+        comment.delete()
+    return redirect('community:detail',question_id = comment.answer.question.id)
