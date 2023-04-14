21f3bb8fbccf0632633864ffd981ece7cf51fb48
alice <alice@modim.co.kr>
2023-04-14 15:43:27 +0900

    03-15 검색 기능 추가하기 -kw를 통해 검색기능 적용 django의 Q함수를 이용해 데이터를 조회한다. - form 엘리먼트를 추가한 이유는 알 수 없음.

--- mysite/community/views/base_views.py
+++ mysite/community/views/base_views.py
@@ -1,6 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-
+from django.db.models import Q
 from ..models import Question
 
 
@@ -10,15 +10,23 @@ def index(request):
     """
     # 입력인자
     page = request.GET.get('page','1')  # 페이지
+    kw = request.GET.get('kw', '')      # 검색어
 
     # 조회
     question_list = Question.objects.order_by('-create_date')
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |
+            Q(content__icontains=kw) |
+            Q(author__username__icontains=kw) |
+            Q(answer__author__username__icontains=kw)
+        ).distinct()
 
     # 페이징 처리
     paginator = Paginator(question_list, 10)    # 페이지당 10개씩 보여 주기
     page_obj = paginator.get_page(page)
 
-    context = {'question_list': page_obj}
+    context = {'question_list': page_obj, 'page':page, 'kw' : kw}
     return render(request,'community/question_list.html', context)
 
 def detail(request, question_id):
